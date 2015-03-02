ba0728a88e518ba6b11f8e2f02ba88d615dcd37f
ashleybridgwood <ashley_bridgwood@hotmail.co.uk>
2015-03-02 13:30:07 +0000

    Added method in factory class for generating gene values

--- EvolutionaryArtStage2/src/main/BioController.java
+++ EvolutionaryArtStage2/src/main/BioController.java
@@ -14,10 +14,19 @@ public class BioController {
 		FileHandler.checkForMainWorkingFolder();
 		
 		Save.saveBiomorph("Test", "1841653102515205");
+		System.out.println(Load.loadBiomorph("Test"));
 		
 		new BioCache();
 		Log.add("BioCache Initiated");
+		
+		while(!BioCache.isEmpty()){
+			String data = BioCache.pop();
+			System.out.println(data);
+		}
+		
 		Log.exportLogToFile();
+	
+		
 	}
 	
 }
--- EvolutionaryArtStage2/src/main/Factory.java
+++ EvolutionaryArtStage2/src/main/Factory.java
@@ -7,6 +7,17 @@
 
 package main;
 
-public class Factory {
+import java.util.Random;
 
+public class Factory {
+	public static int generateGeneValues(){
+		Random r = new Random();
+		int value;
+		
+		do{
+			value = r.nextInt(3) -3;
+		} while(value == 0);
+		
+		return value;	
+	}
 }
