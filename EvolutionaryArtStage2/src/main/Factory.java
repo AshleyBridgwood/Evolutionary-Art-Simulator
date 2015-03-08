a3a2f2ec9c261677d38c6423e32dcef668d3fdda
Matt Gould <gouldie07@hotmail.co.uk>
2015-03-08 21:27:00 +0000

    added seeding to factory class
    Added seeding to Factory class. Init() will be called when a project is
    created, as will generateSeed(). generateGeneValues() can be called for
    every biomorph with the results specific to the seed.

--- EvolutionaryArtStage2/src/main/Factory.java
+++ EvolutionaryArtStage2/src/main/Factory.java
@@ -2,7 +2,7 @@
  * DESCRIPTION OF THE CLASS
  * 
  * @author Ashley Bridgwood
- *
+ * @author Matthew Gould
  */
 
 package main;
@@ -10,21 +10,40 @@ package main;
 import java.util.Random;
 
 public class Factory {
-	
-	private static Random r;
-	
+
+	private static Random rnd;
+	private static int value;
+
 	/**
 	 * Generate random numbers for setting gene values
+	 * 
 	 * @return int
 	 */
-	public static int generateGeneValues(){
-		r = new Random();
-		int value;
-		
-		do{
-			value = r.nextInt(7) -3;
-		} while(value == 0);
-		
-		return value;	
+
+	// called at the start of each project. 
+	public static void init() {
+		rnd = new Random();
+	}
+
+	// generates random seed for project
+	public static long generateSeed() {
+		long seed = System.currentTimeMillis();
+		rnd.setSeed(seed);
+		return seed;
+	}
+
+	// assigns custom seed for project
+	public static long generateSeed(long customSeed) {
+		rnd.setSeed(customSeed);
+		return customSeed;
+	}
+
+	// generate gene values using seed
+	public static int generateGeneValues() {
+		do {
+			value = rnd.nextInt(7) - 3;
+		} while (value == 0);
+
+		return value;
 	}
 }
