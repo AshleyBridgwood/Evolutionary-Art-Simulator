c7f6eddefc16ef6f706d21a81bf88bd166b367f3
James Langford <James@10.76.16.164>
2015-04-27 18:14:45 +0100

    Changed colour class, now contains 4 schemes and a method that allows you to choose a scheme and it will return a random colour from that scheme.

--- EvolutionaryArtStage2/src/model/Colour.java
+++ EvolutionaryArtStage2/src/model/Colour.java
@@ -6,12 +6,24 @@ import java.util.Random;
 public class Colour {
 	
 	private static final String[] colours = {"red","yellow","blue","orange","pink","cyan","magenta","black","lightGrey","darkGrey"};
+	private static final Color[] reds = {new Color(255,0,0), new Color(255,153,0), new Color(255,204,0), new Color(255,255,102)};
+	private static final Color[] blues = {new Color(0,0,68), new Color(0,51,102), new Color(102,153,204), new Color(192,192,192)};
+	private static final Color[] greens = {new Color(0,255,0), new Color(4,193,4), new Color(1,128,1), new Color(0,50,0)};
+	private static final Color[] greyscale = {new Color(0,0,0), new Color(105,105,105), new Color(128,128,128), new Color(169,169,169)};
+	private static final Color[][] colourSchemes = {reds,blues,greens,greyscale};
+
 	
 	public Colour()
 	{
 		
 	}
 	
+	/*
+	 * Method  to return a random colour from the array,
+	 * probably not needed anymore once new features are added in.
+	 * 
+	 * */
+	 
 	public Color getRandomColour()
 	{
 		Random rand = new Random();
@@ -55,5 +67,22 @@ public class Colour {
 		return chosenColor;
 	}
 	
-
+	public Color getRandomColourFromScheme(int choice)
+	{
+		/*
+		 * 0 - reds
+		 * 1 - blues
+		 * 2 - greens
+		 * 3 - greyscale
+		 */
+		
+		Color[] arrayChosen = colourSchemes[choice];
+		Random rand = new Random();
+		int randColour = rand.nextInt(4);
+	
+		return arrayChosen[randColour];
+	}
 }
+	
+
+
