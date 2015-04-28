ec936f7ca2292cf65158a955ca28488dc4bc3518
ashleybridgwood <ashley_bridgwood@hotmail.co.uk>
2015-04-28 17:30:36 +0100

    Tree class commented

--- EvolutionaryArtStage2/src/model/Tree.java
+++ EvolutionaryArtStage2/src/model/Tree.java
@@ -1,3 +1,10 @@
+/**
+ * Tree implementation for use creating each Biomorph
+ * 
+ * @author Ashley Bridgwood
+ *
+ */
+
 package model;
 
 import model.Point;
@@ -8,28 +15,46 @@ import java.util.List;
 
 public class Tree {
 	
-	protected TreeNode<Point> root;
+	protected TreeNode<Point> root; //stores the root node
 
-	// constructs an empty tree
+	/**
+	 * Constructs and empty tree
+	 * @param root Sets the root not of the tree
+	 */
 	public Tree(TreeNode<Point> root){
 		this.root = root;
 	}
 	
 	
-	// reset the tree to empty state
+	/**
+	 * Clears the tree
+	 */
 	public void clear(){
 		root = null;
 	}
 	
-	//Gets the root
+	/**
+	 * Gets the root of the tree
+	 * @return TreeNode<Point> The root node of the tree
+	 */
 	public TreeNode<Point> getRoot(){
 		return root;
 	}
-	
+	/**
+	 * Checks to see if a given key exists within the tree
+	 * @param key Key to search - ID of the node
+	 * @return Boolean True if found, false if not
+	 */
 	public boolean exists(int key){
 		return find(root, key);
 	}
 	
+	/**
+	 * Checks to see if a given key is in a given node
+	 * @param node Tree node which needs searching
+	 * @param key Find the key within the tree
+	 * @return Boolean True if found, false if not
+	 */
 	private boolean find(TreeNode<Point> node, int key){
 		boolean result = false;
 		if(node.element.getID() == key){
@@ -44,13 +69,21 @@ public class Tree {
 		return result;
 	}
 	
-	
+	/**
+	 * Gets the tree in an order
+	 * @return ArrayList<TreeNode<Point>> The ordered tree
+	 */
 	public ArrayList<TreeNode<Point>> getPreOrderTraversal(){
 		ArrayList<TreeNode<Point>> list = new ArrayList<TreeNode<Point>>();
 		buildPreOrder(root, list);
 		return list;
 	}
 	
+	/**
+	 * Sorts the tree in an order
+	 * @param node Is the node which needs sorting
+	 * @param preOrder The existing order tree
+	 */
 	private void buildPreOrder(TreeNode<Point> node, ArrayList<TreeNode<Point>> preOrder){
 		preOrder.add(node);
 		for(TreeNode<Point> child : node.getChildren()){
@@ -58,6 +91,12 @@ public class Tree {
 		}
 	}
 	
+	/**
+	 * Finds and returns a tree node
+	 * @param node Node to search
+	 * @param key Key to find within the tree
+	 * @return TreeNode<Point> the node which needed to be found
+	 */
 	public TreeNode<Point> findTreeNode(TreeNode<Point> node, int key){
 		if(node== null){
 			return null;
@@ -76,62 +115,79 @@ public class Tree {
 		return null;
 	}
 	
-	
-
-	
-	public ArrayList<Line> getLineInformation(){
-		ArrayList<Line> lines = new ArrayList<Line>();
-		
-		if(root != null){
-			
-		}
-			
-		return lines;
-	}
-	
+	//Inner class for the TreeNode
 	protected static class TreeNode<Point> {
 		
-		private Point element;
-		private List<TreeNode<Point>> children;
-		private TreeNode<Point> parent;
-		private int numberOfChildren;
+		private Point element; //Data which is stored within each TreeNode
+		private List<TreeNode<Point>> children; //A list of all children which are connected to the TreeNode
+		private TreeNode<Point> parent; //Parent TreeNode of the current TreeNode
+		private int numberOfChildren; //Number of children which the TreeNode has
 		
-		// Constructor for creating a leaf node
+		/**
+		 * Constructor for the new TreeNode
+		 * @param element 
+		 */
 		public TreeNode(Point element){
 			this.element = element;
-			
 			children = new ArrayList<TreeNode<Point>>();
 		}
 		
-		// Constructor for creating a parent node
+		/**
+		 * Constructor for a new TreeNode
+		 * @param node Set the new node the same as the node passed in
+		 */
 		public TreeNode(TreeNode<Point> node){
 			this.element = node.element;
 			numberOfChildren = 0;
 			children = new ArrayList<TreeNode<Point>>();
 		}
 		
+		/**
+		 * Sets the element within the TreeNode
+		 * @param element Data which needs to be set
+		 */
 		public void setElement(Point element){
 			this.element = element;
 		}
 		
+		/**
+		 * Gets the element out of the TreeNode
+		 * @return the element out of the TreeNode
+		 */
 		public Point getElement(){
 			return element;
 		}
 		
+		/**
+		 * Adds a child to the TreeNode
+		 * @param child Child TreeNode which needs adding to the TreeNode
+		 */
 		public void addChild(TreeNode<Point> child){
 			child.setParent(this);
 			children.add(child);
 			numberOfChildren++;
 		}
 		
+		/**
+		 * Gets the parent from the TreeNode
+		 * @return the parent from the TreeNode
+		 */
 		public TreeNode<Point> getParent(){
 			return this.parent;
 		}
 		
+		/**
+		 * Gets the child from the TreeNode
+		 * @return a list of TreeNode containing the children
+		 */
 		public List<TreeNode<Point>> getChildren(){
 			return this.children;
 		}
 		
+		/**
+		 * Sets the children of the TreeNode
+		 * @param children List of children which will be assigned to the TreeNode
+		 */
 		public void setChildren(List<TreeNode<Point>> children){
 			for(TreeNode<Point> child : children){
 				child.setParent(this);
@@ -139,11 +195,12 @@ public class Tree {
 			this.children = children;
 		}
 		
-		
+		/**
+		 * Sets the parent of the TreeNode
+		 * @param parent TreeNode to be set as the Parent of the TreeNode
+		 */
 		public void setParent(TreeNode<Point> parent){
 			this.parent = parent;
 		}
-		
 	}
-
 }
