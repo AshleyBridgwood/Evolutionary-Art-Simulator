e56e6ca0889dfbbb421c3e33705b82cd3e56588b
ashleybridgwood <ashley_bridgwood@hotmail.co.uk>
2015-04-28 11:20:23 +0100

    BioCache cleaned up and commented

--- EvolutionaryArtStage2/src/model/BioCache.java
+++ EvolutionaryArtStage2/src/model/BioCache.java
@@ -1,5 +1,5 @@
 /**
- * DESCRIPTION OF THE CLASS
+ * Stores the parent and children biomorphs of each process in the program
  * 
  * @author Ashley Bridgwood
  *
@@ -9,38 +9,61 @@ package model;
 
 public class BioCache {
 
-	private static Object[] stack;
-	private static int defaultValue;
-	private static int top;
+	private static Object[] stack; //Stores the stack used for the undo feature
+	private static int defaultValue; //Number of undo's which the program allows
+	private static int top; //Stores the number of items in the stack
 	
 	public BioCache() {
+		//Initialise the fields
 		top = -1;
-		defaultValue = 200;
+		defaultValue = 1000;
 		stack = new Object[defaultValue];
 	}
 	
+	/**
+	 * Pushes a new set of parent and children biomorphs onto the stack
+	 * @param newValue Parent and child biomorphs to be added
+	 */
 	public static void push(Object newValue){
 		stack[++top] = newValue;
-		Log.add("Biomorph data added to the cache");
 	}
 	
+	/**
+	 * Removes the data on the top of the stack
+	 * @return Object The data which has been removed
+	 */
 	public static Object pop(){
-		Log.add("Biomorph data removed from the cache");
 		return stack[top--];
 	}
 	
+	/**
+	 * Checks to see if the stack is empty
+	 * @return Boolean true is empty, false if not
+	 */
 	public static boolean isEmpty(){
 		return (top == -1);
 	}
 	
+	/**
+	 * Checks to see if the stack is full
+	 * @return Boolean true if full, false if not
+	 */
 	public static boolean isFull(){
 		return (top == defaultValue -1);
 	}
 	
+	/**
+	 * Gets the top value of the stack without removing it
+	 * @return Object The parent and children at the top of the stack
+	 */
 	public static Object peek(){
 		return stack[top];
 	}
 	
+	/**
+	 * Gets the number of items which are on the stack
+	 * @return int The numnber of items in the stack
+	 */
 	public static int getNumberOfItemsOnStack(){
 		return top;
 	}
