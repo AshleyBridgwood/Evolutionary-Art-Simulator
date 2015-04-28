a3c4fdbc456b9e98b675dd6dae2bf93dceeb6b72
ashleybridgwood <ashley_bridgwood@hotmail.co.uk>
2015-04-28 19:35:48 +0100

    Load class commented

--- EvolutionaryArtStage2/src/model/Load.java
+++ EvolutionaryArtStage2/src/model/Load.java
@@ -1,5 +1,5 @@
 /**
- * DESCRIPTION OF THE CLASS
+ * Load - Deal with loading of files and deserialising objects which are saved to file
  * 
  * @author Ashley Bridgwood
  *
@@ -9,23 +9,35 @@ package model;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class Load extends FileHandler {
 	
+	/**
+	 * Retrieves a set of biomorphs from file
+	 * @param fileName name of the file to be loaded
+	 * @return ArrayList<ArrayList<Line>> the loaded parent and children data
+	 */
 	public static ArrayList<ArrayList<Line>> loadAllBiomorphs(String fileName){
 		return FileHandler.loadBiomorphs("\\Saved Biomorphs\\" + fileName + ".biomorph");
 	}
 	
+	/**
+	 * Loads the hall of fame biomorphs from file
+	 * @param fileName name of the file to be loaded
+	 * @return ArrayList<Line> the loaded Biomorph
+	 */
 	public static ArrayList<Line> loadHallOfFameBiomorph(String fileName){
 		return FileHandler.loadHallOfFameBiomorph("\\Hall Of Fame\\" + fileName + ".biomorph");
 	}
 	
+	/**
+	 * Loading of a file
+	 * @param fileName name of the file to be loaded
+	 * @return String data out of the file
+	 */
 	public static String loadFile(String fileName){
 		String data = "";
 		if(fileName != null){
