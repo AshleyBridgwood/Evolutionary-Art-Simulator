074970b6132b143b4bdc0d47c50210b6491dee3f
ashleybridgwood <ashley_bridgwood@hotmail.co.uk>
2015-05-02 19:44:28 +0100

    Added BioCache test

--- EvolutionaryArtStage2/src/testing/TestBioCache.java
+++ EvolutionaryArtStage2/src/testing/TestBioCache.java
@@ -1,5 +1,36 @@
 package testing;
 
-public class TestBioCache {
+import static org.junit.Assert.*;
+import model.BioCache;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBioCache {
+	
+	private Object data;
+	
+	@Test
+	public void testPush(){
+		BioCache cache = new BioCache();
+		assertEquals(cache.getNumberOfItemsOnStack(), -1);
+		Object data = null;
+		cache.push(data);
+		assertEquals(cache.getNumberOfItemsOnStack(), 0);
+		cache.push(data);
+		cache.push(data);
+		assertEquals(cache.getNumberOfItemsOnStack(), 2);
+	}
+	
+	@Test
+	public void testPop(){
+		BioCache cache = new BioCache();
+		for(int i = 0; i < 10; i++){
+			cache.push(data);
+		}
+		assertEquals(cache.getNumberOfItemsOnStack(), 9);
+		cache.pop();
+		cache.pop();
+		assertEquals(cache.getNumberOfItemsOnStack(), 7);
+	}
 }
